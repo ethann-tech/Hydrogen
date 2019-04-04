2ec3b8c646dbfdf4d2d0d6537139b41a7cc9381d
Hayquan <wonium@qq.com>
2019-04-04 20:06:33 +0800

    添加layout_toolbar

--- app/src/main/java/com/wonium/aclj/ui/MainActivity.java
+++ app/src/main/java/com/wonium/aclj/ui/MainActivity.java
@@ -51,6 +51,7 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void initView() {
+
         StatusBarUtil.getInstance().setColor(this,getResources().getColor(R.color.aliceBlue));
         initFragment();
 
---
+++ app/src/main/java/com/wonium/aclj/ui/activity/ProgressActivity.java
@@ -0,0 +1,32 @@
+package com.wonium.aclj.ui.activity;
+
+import com.wonium.aclj.ui.BaseActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProgressActivity extends BaseActivity {
+    @Override
+    public void initWindowAttributes() {
+
+    }
+
+    @Override
+    public int getLayoutResId() {
+        return 0;
+    }
+
+    @Override
+    public void bindLayout(int layoutResId) {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initListener() {
+
+    }
+}
